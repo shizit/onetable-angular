f87ea2d47e783381e0c5d9e116797a36e08753a8
shizit <shizit1995@gmail.com>
2023-05-15 23:48:07 +0900

    chore: add angular-material

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import {
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import routeConfig from './app/routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
+  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig), provideAnimations()],
 }).catch((err) => console.error(err));
